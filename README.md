0a2f667fa36e4a4e51274fa28a06c58e0f6b724a
Wesley Hiromitsu <wes.hiro@gmail.com>
2019-11-07 16:11:27 -0300

    login page + animation

--- README.md
+++ README.md
@@ -10,8 +10,15 @@
 
 ## Installation
 
+Clone repository
+
 ```sh
 git clone https://github.com/WesleyHiromitsu/ReactJS_Base_WithLogin.git
+``` 
+
+Install package
+
+```sh
 npm install
 ``` 
 
@@ -27,3 +34,5 @@ npm install
 [npm-image]: https://img.shields.io/npm/v/datadog-metrics.svg?style=flat-square
 [npm-url]: https://npmjs.org/package/datadog-metrics
 [npm-downloads]: https://img.shields.io/npm/dm/datadog-metrics.svg?style=flat-square
+[travis-image]: https://img.shields.io/travis/dbader/node-datadog-metrics/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/dbader/node-datadog-metrics
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,6 +7,10 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+//grid
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default class Login extends Component {
   constructor(props) {
@@ -48,32 +52,46 @@ export default class Login extends Component {
 
   render(){
     return (
-      <div className="centralize">
-        <Container>
-        {
-          this.state.message !== ''? (
-            <Alert variant="danger">{this.state.message}</Alert>
-          ) : ''
-        }
-          <Form onSubmit={this.signIn}>
+      <Container>
+        <Row>
+          <Col>
+            
+            <div>
+              {
+                this.state.message !== ''? (
+                  <Alert variant="danger">{this.state.message}</Alert>
+                ) : ''
+              }
+                <Form className="form-layout" onSubmit={this.signIn}>
 
-            <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" onChange={e => this.email = e.target.value} placeholder="Digite seu emai" />
-            </Form.Group>
+                  <div className="form-header">
+                    <h1>Bem Vindo</h1>
+                    <p>Coloque a logo no local do bem vindo!</p>
+                  </div>
 
-            <Form.Group>
-              <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" onChange={e => this.password = e.target.value} placeholder="Digite sua senha" />
-            </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" onChange={e => this.email = e.target.value} placeholder="Digite seu emai" />
+                  </Form.Group>
 
-            <Button variant="primary" block onClick={this.signIn}>
-              Entrar
-            </Button>
+                  <Form.Group>
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" onChange={e => this.password = e.target.value} placeholder="Digite sua senha" />
+                  </Form.Group>
 
-          </Form>
-        </Container>
-      </div>
+                  <Button className="botao" variant="primary" block onClick={this.signIn}>
+                    Entrar
+                  </Button>
+
+                  <div className="form-footer">
+                    <h1>Esqueceu a senha?</h1><a href="#"> Clique aqui</a>
+                  </div>
+                </Form>
+            </div>
+          
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
